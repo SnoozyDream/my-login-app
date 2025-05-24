108bd739d831f087257ad8d46584b6999c669877
SnoozyDream <vrcsnzy@gmail.com>
2025-05-24 11:57:50 +0900

    登録、削除処理追加

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,10 +4,27 @@ import { useNavigate } from 'react-router-dom';
 function Dashboard() {
   const navigate = useNavigate();
 
+//仮の勉強ログ
+const studyLogs = [
+  { date: '2025/05/21', title: '移動平均線について',time:30},
+  { date: '2025/05/20', title: 'ローソク足の読み方',time:15}
+]
+
   return (
-    <div>
+    <div style={{ padding: '2rem' }}>
       <h2>ダッシュボード</h2>
       <button onClick={() => navigate('/register')}>登録画面へ</button>
+
+      <hr />
+
+      <h3>勉強履歴一覧</h3>
+      <ui>
+        {studyLogs.map((log, index)=> (
+          <li key={index}>
+            ・{log.date} -{log.title} ({log.time}分)
+          </li>
+        ))}
+      </ui>
     </div>
   );
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function RegisterPage() {
+  const [title,setTitle] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = () => {
-    // 仮の登録処理
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // 仮の処理（あとで状態管理 or DBに送るようにする）
+    console.log("記録:", { title, date, time });
+
+    //登録後にダッシュボードへ戻る
     navigate('/dashboard');
   };
 
   return (
-    <div>
-      <h2>登録画面</h2>
-      <button onClick={handleSubmit}>記録する</button>
+    <div style={{ padding: '2rem' }}>
+      <h2>学習内容の登録</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>学習内容:</label><br />
+          <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          />
+        </div>
+        <div>
+          <label>日付:</label><br />
+          <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          required
+          />
+        </div>
+        <div>
+          <label>勉強時間(分) :</label><br />
+          <input
+          type="number"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+          required
+          />
+        </div>
+        <br />
+        <button onClick={handleSubmit}>記録する</button>
+      </form>
     </div>
   );
 }
